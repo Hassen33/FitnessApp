f7036f8358d415e2c615ccac2ef9a6248a2e1c08
hassen <hassenbj33@gmail.com>
2022-07-26 18:05:22 +0100

    HeroBanner

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
 
 import './App.css';
-// import ExerciseDetail from './pages/ExerciseDetail';
-// import Home from './pages/Home';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
@@ -12,8 +12,8 @@ const App = () => (
   <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
     <Navbar />
     <Routes>
-      {/* <Route path="/" element={<Home />} />
-      <Route path="/exercise/:id" element={<ExerciseDetail />} /> */}
+      <Route path="/" element={<Home />} />
+      {/* <Route path="/exercise/:id" element={<ExerciseDetail />} /> */}
     </Routes>
     <Footer />
   </Box>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,41 @@
-import React, { useState } from 'react';
-import { Box } from '@mui/material';
+// import React, { useState } from 'react';
+// import { Box } from '@mui/material';
 
+// import Exercises from '../components/Exercises';
+// import SearchExercises from '../components/SearchExercises';
+// import HeroBanner from '../components/HeroBanner';
+
+// const Home = () => {
+//   const [exercises, setExercises] = useState([]);
+//   const [bodyPart, setBodyPart] = useState('all');
+
+//   return (
+//     <Box>
+//       <HeroBanner />
+//       <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+//       <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
+//     </Box>
+//   );
+// };
+
+// export default Home;
+import React, { usestate } from 'react';
+import { Box } from '@mui/material';
 import Exercises from '../components/Exercises';
 import SearchExercises from '../components/SearchExercises';
 import HeroBanner from '../components/HeroBanner';
 
 const Home = () => {
-  const [exercises, setExercises] = useState([]);
-  const [bodyPart, setBodyPart] = useState('all');
-
   return (
     <Box>
-      <HeroBanner />
-      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
-      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
-    </Box>
-  );
-};
+   
+   <HeroBanner />
+  {/*
+  
+  <SearchExercises />
+  <Exercises /> */}
+  </Box>
+  )
+}
 
-export default Home;
+export default Home
